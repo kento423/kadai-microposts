5bca2f49c21dce77c3f90626a1629be0cb7d9eb3
kento.okamoto <mntymnty6@gmail.com>
2022-11-16 11:52:09 +0000

    feature favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @pagy, @favoposts = pagy(@user.favoposts)
+    counts(@user)
+  end
+  
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favoposts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -29,5 +31,19 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def add_to_favorites(micropost)
+    # 自分自身の投稿もお気に入りできる
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def remove_from_favorites(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favoring?(micropost)
+    self.favoposts.include?(micropost)
   end
 end
